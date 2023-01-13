f830e5827e2ccbb00a876248d48e79c0e975dce3
bmcyver <110798010+bmcyver@users.noreply.github.com>
2023-01-14 00:39:44 +0900

    Create example.js

---
+++ example/example.js
@@ -0,0 +1,29 @@
+// 자세한 API의 사용방법은 https://developers.naver.com 을 참고하세요.
+
+const naverAPI = require('naver-openAPI-wrapper');
+const naver = new naverAPI();
+naver.init('your client id', 'your client key');
+
+// shortURL(string)
+naver.shortURL('URL', false);
+
+// translation(string)
+naver.translation('source', 'target', 'text', false);
+
+// detect Languages(string)
+naver.detectLangs('text');
+
+// romanize(object)
+naver.romanization('text', false);
+
+// searchBlog()
+naver.searchBlog('query', 1, false);
+
+// searchBook
+naver.searchBook('query', 1, false);
+
+// searchBook
+naver.searchBook('query', 1, false);
+
+// is adult keyword
+naver.searchAdult('query');
